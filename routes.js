a5b1dbdb4def87ca77b82806055877de0ed794b4
Aravind Ariharasudhan <100225369+arihara-sudhan@users.noreply.github.com>
2023-08-18 13:20:55 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const postModel = require('./schemas/post');
 
 router.get('/', (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', "true");
-  res.send("API IS RUNNING.......................");
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.send('API IS RUNNING.......................');
 });
 
 router.post('/createPost', async (req, res) => {
@@ -14,7 +14,7 @@ router.post('/createPost', async (req, res) => {
       imgurl: req.body.imgurl || null,
       feeds: req.body.feeds,
       num: req.body.num,
-      topic: req.body.topic
+      topic: req.body.topic,
     });
     const saveItem = await newItem.save();
     res.send(newItem);
@@ -27,6 +27,22 @@ router.get('/getPosts', async (req, res) => {
   try {
     const allItems = await postModel.find({});
     res.status(200).json(allItems);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// New route to get a single post by num
+router.get('/getPosts/:num', async (req, res) => {
+  try {
+    const num = req.params.num;
+    const post = await postModel.findOne({ num: num });
+    
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: 'Post not found' });
+    }
   } catch (err) {
     res.json(err);
   }
